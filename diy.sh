f77c689f5fdc8dddd17337b4ac209624e94d1f70
getenjr <32555548+getenjr@users.noreply.github.com>
2025-01-11 02:54:54 +0700

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,12 +6,12 @@
 # https://github.com/kenzok8
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
 rm -rf package/lean/luci-theme-argon
 sed -i 's/LuCI Master/LuCI master by kenzo/g' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
+echo 'luciversion = "23.05"' >> /usr/lib/lua/luci/version.lua
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 ./scripts/feeds update -a
 ./scripts/feeds install
